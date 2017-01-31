32b56a5b1bc01c972c8928204ebe656511b1a6d7
Jonathan De Heus <jdeheus2@gmail.com>
2017-01-30 20:59:07 -0500

    First commit. Uploading what I currently have to Github.

---
+++ js/customjs.js
@@ -0,0 +1,26 @@
+/*Checks initial window size & applies relevant classes if necessary*/
+$(document).ready(function () {
+  var WinWidth = $(window).width();
+  if (WinWidth < 640) {
+    $("#prev").removeClass( "uk-margin-medium-top" );
+    $("#next").addClass("uk-margin-small-bottom");
+    $("#prev").addClass("uk-margin-small-top");
+  }
+});
+
+/*Adds or subtracts classes as window is resized*/
+$(window).resize(function(event){
+  var WinWidth = $(window).width();
+  if (WinWidth < 640) {
+    $("#prev").removeClass( "uk-margin-medium-top" );
+    $("#next").addClass("uk-margin-small-bottom");
+    $("#prev").addClass("uk-margin-small-top");    
+  } else {
+    $("#prev").addClass("uk-margin-medium-top");
+    $("#next").removeClass("uk-margin-small-bottom");
+  }
+});
+
+/*$(window).resize(function(event){
+  var winWidth = $
+});*/
