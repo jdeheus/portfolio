8e791a62197f3339a5384e3f7715cecc6ce82a3f
Jonathan De Heus <jdeheus2@gmail.com>
2017-02-05 11:39:31 -0500

    Updated CSS & JS files. Changed index.html so that there's a cleaner break between displays under 640px, and those above them. Changed page_template so that there is now a display for 3 media items along with the previously mentioned 1 & 2 media items.

--- js/customjs.js
+++ js/customjs.js
@@ -1,4 +1,4 @@
-/*Checks initial window size & applies relevant classes if necessary*/
+//Checks initial window size & applies relevant classes if necessary
 $(document).ready(function () {
   var WinWidth = $(window).width();
   if (WinWidth < 640) {
@@ -8,19 +8,15 @@ $(document).ready(function () {
   }
 });
 
-/*Adds or subtracts classes as window is resized*/
+//Adds or subtracts classes as window is resized
 $(window).resize(function(event){
   var WinWidth = $(window).width();
   if (WinWidth < 640) {
     $("#prev").removeClass( "uk-margin-medium-top" );
     $("#next").addClass("uk-margin-small-bottom");
-    $("#prev").addClass("uk-margin-small-top");    
+    $("#prev").addClass("uk-margin-small-top");
   } else {
     $("#prev").addClass("uk-margin-medium-top");
     $("#next").removeClass("uk-margin-small-bottom");
   }
 });
-
-/*$(window).resize(function(event){
-  var winWidth = $
-});*/
