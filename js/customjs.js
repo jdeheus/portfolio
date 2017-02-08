dedf181dc18714ba5dcbe3e12e7c1bc0a8b93c72
Jonathan De Heus <jdeheus2@gmail.com>
2017-02-08 17:04:50 -0500

    Made multiple changes to all HTML pages, as well as the CSS & the JS. Updated  the icons on the homepage so that they now expand whether you mouse over the picture or the button, and the button will toggle to the hover state even if you hover over the picture. Changed the size of the logo in the navbar. Changed the break points of the columns on the resume page. Changed the hero text color on  the about me, resume, and page template pages. Added a 4 up area on the page template, and changed some breakpoints.

--- js/customjs.js
+++ js/customjs.js
@@ -20,3 +20,25 @@ $(window).resize(function(event){
     $("#next").removeClass("uk-margin-small-bottom");
   }
 });
+
+//Activates overlay and button no matter what you're hovering over in the card.
+$(document).ready(function () {
+  $('.uk-overflow-hidden').hover(function () {
+        $(this).parent().find('button').css('border-color','#b2b2b2');
+     },
+
+     function () {
+        $(this).parent().find('button').css('border-color','#e5e5e5');
+     }
+   );
+  $('button').hover(function () {
+        $(this).parent().find('div').addClass('uk-hover');
+        $(this).css('border-color','#b2b2b2');
+     },
+
+     function () {
+        $(this).parent().find('div').removeClass('uk-hover');
+        $(this).css('border-color','#e5e5e5');
+     }
+   );
+  });
